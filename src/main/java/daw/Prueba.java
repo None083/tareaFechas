86bbb484a842a564020a450927971e5ff1ee87a9
Noelia <perezgonzaleznoelia08@gmail.com>
2023-11-27 18:31:52 +0100

    terminado

--- src/main/java/daw/Prueba.java
+++ src/main/java/daw/Prueba.java
@@ -4,7 +4,10 @@
  */
 package daw;
 
+import static daw.UtilidadesFecha.pedirNumero;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  *
@@ -13,13 +16,139 @@ import java.time.LocalDate;
 public class Prueba {
 
     public static void main(String[] args) {
-       
-        LocalDate fecha = LocalDate.of(2023, 11, 26);
+
+        System.out.println("Introduce el día");
+        int day = pedirNumero();
+
+        System.out.println("Introduce el mes");
+        int month = pedirNumero();
+
+        System.out.println("Introduce el año");
+        int year = pedirNumero();
+
+        LocalDate fecha = LocalDate.of(year, month, day);
         
-        System.out.println(UtilidadesFecha.diaSemana(fecha));
+        int opcion = 0;
         
-        System.out.println(UtilidadesFecha.mostrarFechaLarga(fecha));
+        do{
+            System.out.println("""
+                           
+                                            ¿Qué quieres hacer?
+                           -------------------------------------------------------
+                                    1 - ¿Es bisiesto?
+                                    2 - Fecha válida
+                                    3 - Crear copia
+                                    4 - Número de días del mes
+                                    5 - Número del día de la semana
+                                    6 - Fecha larga
+                                    7 - Sumar días
+                                    8 - Días transcurridos entre dos fechas
+                                    9 - Día siguiente
+                                   10 - Día anterior
+                                   11 - Minutos entre dos fechas
+                                   12 - Sumar segundos
+                                   13 - Salir
+                           -------------------------------------------------------
+                           """);
+        opcion = UtilidadesFecha.pedirNumero();
+
+        switch (opcion) {
+            case 1 -> {
+                if (UtilidadesFecha.bisiesto(fecha)) {
+                    System.out.println("El año " + year + " es bisiesto");
+                } else {
+                    System.out.println("El año " + year + " no es bisiesto");
+                }
+            }
+            case 2 -> {
+                if (UtilidadesFecha.fechaValida(day, month, year)) {
+                    System.out.println("La fecha " + fecha + " es válida");
+                } else {
+                    System.out.println("La fecha " + fecha + " no es válida");
+                }
+            }
+            case 3 -> {
+                LocalDate copiaFecha = UtilidadesFecha.copia(fecha);
+
+                if (fecha.equals(copiaFecha)) {
+                    System.out.println("La copia se ha realizado con éxito");
+                } else {
+                    System.out.println("Algo ha fallado");
+                }
+            }
+            case 4 -> {
+                System.out.println("El número de días del mes son: "
+                        + UtilidadesFecha.diasMes(fecha));
+            }
+            case 5 -> {
+                System.out.println("El número del día dentro de la semana es: "
+                        + UtilidadesFecha.diaSemana(fecha));
+            }
+            case 6 -> {
+                System.out.println("La versión larga de la fecha es: "
+                        + UtilidadesFecha.mostrarFechaLarga(fecha));
+            }
+            case 7 -> {
+                System.out.println("Introduce un número de días a sumar");
+                int numDias = UtilidadesFecha.pedirNumero();
+                System.out.println("Fecha: " + fecha + " Resultado: " + UtilidadesFecha.sumarDias(numDias, fecha));
+            }
+            case 8 -> {
+                System.out.println("Nueva fecha");
+                System.out.println("Introduce el día");
+                int day2 = pedirNumero();
+
+                System.out.println("Introduce el mes");
+                int month2 = pedirNumero();
+
+                System.out.println("Introduce el año");
+                int year2 = pedirNumero();
+
+                LocalDate fecha2 = LocalDate.of(year2, month2, day2);
+                
+                System.out.println("Los días transcurridos entre " + fecha + " y " + fecha2
+                        + " son: " + UtilidadesFecha.diasTranscurridos(fecha, fecha2));
+            }
+            case 9 ->{
+                System.out.println("Día siguiente de la fecha " + fecha
+                + ": " + UtilidadesFecha.siguienteDia(fecha));
+            }
+            case 10 ->{
+                System.out.println("Día anterior de la fecha " + fecha
+                + ": " + UtilidadesFecha.anteriorDia(fecha));
+            }
+            case 11 ->{
+                System.out.println("Nueva fecha");
+                System.out.println("Introduce el día");
+                int day2 = pedirNumero();
+
+                System.out.println("Introduce el mes");
+                int month2 = pedirNumero();
+
+                System.out.println("Introduce el año");
+                int year2 = pedirNumero();
+
+                LocalDate fecha2 = LocalDate.of(year2, month2, day2);
+                
+                System.out.println("Los minutos entre la fecha " + fecha + " y "
+                + fecha2 + " son: " + UtilidadesFecha.minutosEntre(
+                        LocalDateTime.of(fecha, LocalTime.MIN), 
+                        LocalDateTime.of(fecha2, LocalTime.MIN)));
+            }
+            case 12 ->{
+                System.out.println("Introduce un número de segundos a sumar");
+                int numSeg = UtilidadesFecha.pedirNumero();
+                
+                System.out.println("Fecha: " + fecha + " Resultado: " + UtilidadesFecha.sumarSegundos(numSeg, 
+                        LocalDateTime.of(fecha, LocalTime.MIN)));
+            }
+            default ->{
+                System.out.println("Chao pescao");
+            }
+        }
+        }while(opcion != 13);
         
+
     }
-    
+
 }
--- src/main/java/daw/UtilidadesFecha.java
+++ src/main/java/daw/UtilidadesFecha.java
@@ -1,16 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw;
 
 import java.time.DateTimeException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Month;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.InputMismatchException;
 import java.util.Locale;
+import java.util.Scanner;
 
 /**
  *
@@ -18,83 +19,95 @@ import java.util.Locale;
  */
 public class UtilidadesFecha {
 
-    public static boolean bisiesto(LocalDate fecha){
-        
+    public static boolean bisiesto(LocalDate fecha) {
         boolean esBisiesto = false;
-        
-        if (fecha.isLeapYear()){
+
+        if (fecha.isLeapYear()) {
             esBisiesto = true;
         }
-        
         return esBisiesto;
     }
-    
-    public static boolean fechaValida(int day, int month, int year){
-        
-         try{
-             LocalDate fecha = LocalDate.of(year, month, day);
-             return true;
-             
-         }catch(DateTimeException dte){
-             
-             return false;
-         }
-    }    
-    
-    public static LocalDate copia(LocalDate fecha){
-        
-        LocalDate copiaFecha = LocalDate.of(fecha.getYear(), 
+
+    public static boolean fechaValida(int day, int month, int year) {
+        try {
+            LocalDate fecha = LocalDate.of(year, month, day);
+            return true;
+        } catch (DateTimeException dte) {
+            return false;
+        }
+    }
+
+    public static LocalDate copia(LocalDate fecha) {
+        LocalDate copiaFecha = LocalDate.of(fecha.getYear(),
                 fecha.getMonthValue(), fecha.getDayOfMonth());
-        
         return copiaFecha;
     }
-    
-    public static int diasMes(LocalDate fecha){
-        
+
+    public static int diasMes(LocalDate fecha) {
         return fecha.lengthOfMonth();
-        
     }
-    
-    public static int diaSemana(LocalDate fecha){
-        
-        if(fecha.getDayOfWeek().equals(DayOfWeek.SUNDAY)){
+
+    public static int diaSemana(LocalDate fecha) {
+        if (fecha.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
             return 0;
-        }else{
+        } else {
             return fecha.getDayOfWeek().getValue();
         }
-        
     }
-    
-    public static String mostrarFechaLarga(LocalDate fecha){
+
+    public static String mostrarFechaLarga(LocalDate fecha) {
         DateTimeFormatter formato = DateTimeFormatter.ofPattern(
                 "EEEE d 'de' MMMM 'de' yyyy", new Locale("es", "ES"));
         return fecha.format(formato);
     }
-    
-    public static LocalDate sumarDias(long numeroDias, LocalDate fecha){
+
+    public static LocalDate sumarDias(long numeroDias, LocalDate fecha) {
         LocalDate nuevaFecha = fecha.plusDays(numeroDias);
         return nuevaFecha;
     }
-    
-    public static long diasTranscurridos(LocalDate fechaInicial, LocalDate fechaFinal){
-        
+
+    public static long diasTranscurridos(LocalDate fechaInicial, LocalDate fechaFinal) {
+
         return ChronoUnit.DAYS.between(fechaInicial, fechaFinal);
     }
-    
-    public static LocalDate siguienteDia(LocalDate fecha){
+
+    public static LocalDate siguienteDia(LocalDate fecha) {
         final int DIA_SUMAR = 1;
         LocalDate nuevaFecha = fecha.plusDays(DIA_SUMAR);
         return nuevaFecha;
     }
-    
-    public static LocalDate anteriorDia(LocalDate fecha){
+
+    public static LocalDate anteriorDia(LocalDate fecha) {
         final int DIA_RESTAR = 1;
         LocalDate nuevaFecha = fecha.minusDays(DIA_RESTAR);
         return nuevaFecha;
     }
+
+    public static long minutosEntre(LocalDateTime fecha1, LocalDateTime fecha2) {
+        return ChronoUnit.MINUTES.between(fecha1, fecha2);
+    }
+
+    public static LocalDateTime sumarSegundos(long numeroSegundos, LocalDateTime fecha) {
+        //int fechaSegundos = fecha.toLocalTime().toSecondOfDay(); Pasar fecha a segundos
+        LocalDateTime nuevaFecha = fecha.plusSeconds(numeroSegundos);
+        return nuevaFecha;
+    }
     
-    public static long minutosEntre(LocalDateTime fecha1, LocalDateTime fecha2){
-        
+    public static int pedirNumero() {
+        Scanner teclado = new Scanner(System.in);
+        int numeroAnimales = 0;
+        boolean errorMismatch = true;
+        do {
+            try {
+                numeroAnimales = teclado.nextInt();
+                errorMismatch = false;
+            } catch (InputMismatchException ime) {
+                System.out.println("Eso no es un número");
+            }
+            teclado.nextLine();
+        } while (errorMismatch);
+
+        return numeroAnimales;
     }
     
 }
